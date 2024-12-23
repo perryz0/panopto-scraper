caba8bc14cf477b511f76af117e58c9c5415858a
Perry Chien <peiruchien1@gmail.com>
2024-12-22 22:04:57 -0800

    browser visibility for debugging

--- src/script.ts
+++ src/script.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
 // Launch the browser and open a new blank page
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
